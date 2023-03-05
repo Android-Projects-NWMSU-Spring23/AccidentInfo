4068d469073afd6f73d28210142374ad17aa6a63
S556370 <S556370@nwmissouri.edu>
2023-03-04 19:03:40 -0600

    Added landing page for the users after login

---
+++ app/src/main/java/com/example/sundari/accidentinfo/HomeActivity.java
@@ -0,0 +1,128 @@
+package com.example.sundari.accidentinfo;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button logout, prepReport, accessFile;
+    private TextView textView;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        logout = findViewById(R.id.btnLogout);
+        prepReport = findViewById(R.id.btn_PrepReport);
+        accessFile = findViewById(R.id.btn_AccessFile);
+        textView = findViewById(R.id.textView);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+
+        if(firebaseAuth.getUid() != null) {
+            databaseReference = firebaseDatabase.getReference("User").child(firebaseAuth.getUid());
+        }
+
+        progressDialog.setMessage("Loading!");
+        progressDialog.show();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                if (userProfile != null) {
+                    textView.setText("Welcome , " + userProfile.getUserName() );
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(HomeActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Logout();
+            }
+        });
+
+        prepReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(HomeActivity.this , ReportPrepareActivity.class));
+            }
+        });
+
+        accessFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(HomeActivity.this ,AccessFileActivity.class ));
+            }
+        });
+
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.profileMenu:
+                startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
+                break;
+            case R.id.changePassMenu:
+                startActivity(new Intent(HomeActivity.this , ChangePwdActivity.class));
+                break;
+
+            case R.id.logoutMenu:
+                Logout();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}

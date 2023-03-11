43deeb11dba75e0b7ca71091892feded86005aa0
Rangaprudhviraju <82215129+Rangaprudhviraju@users.noreply.github.com>
2023-03-11 16:33:45 -0600

    Enhancement : Preparing report functionality.

---
+++ app/src/main/java/com/example/sundari/accidentinfo/ReportPrepareActivity.java
@@ -0,0 +1,14 @@
+package com.example.sundari.accidentinfo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportPrepareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_prepare);
+    }
+}

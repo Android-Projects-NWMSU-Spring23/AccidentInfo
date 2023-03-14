c64d48e64d1a3d8b479395190db2ae0b5b462169
Bindusree-Penamalli <112150519+Bindusree-Penamalli@users.noreply.github.com>
2023-03-14 15:55:32 -0500

    Enhancement: Added view report functionality.

---
+++ app/src/main/java/com/example/sundari/accidentinfo/AccessFileActivity.java
@@ -0,0 +1,124 @@
+package com.example.sundari.accidentinfo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class AccessFileActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private DatabaseReference databaseReference;
+    private ArrayList<String> fileNames;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access_file);
+
+        listView = findViewById(R.id.lv_fileNames);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("information");
+        fileNames = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fileNames);
+
+
+      /*  databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                fileNames.add(dataSnapshot.getKey());
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(AccessFileActivity.this , databaseError.toString() , Toast.LENGTH_SHORT).show();
+            }
+        }); */
+
+
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                // Info info = new Info();
+
+                fileNames.add(dataSnapshot.getKey());
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(AccessFileActivity.this , databaseError.toString() , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(AccessFileActivity.this , InformationActivity.class);
+                intent.putExtra("fileName" , listView.getItemAtPosition(i).toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home :
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
